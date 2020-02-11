5bd53669270e95d0d67fcf3363a81aeff2dd0270
jaemcu <cupp.j.e@gmail.com>
2020-02-10 20:31:14 -0500

    Feature: Added 'imageurl' column to recipes
    1. Added imageurl to recipe_controller
    2. added imageurl to recipe_serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class RecipeSerializer < ActiveModel::Serializer
-  attributes  :id, :title, :time, :servings, :ingredients, :instructions
+  attributes  :id, :title, :time, :servings, :ingredients, :instructions,
+              :imageurl
   belongs_to :user
   def editable
     scope == object.user
